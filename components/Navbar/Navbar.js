4573a2e58474a3c586f039c23a566c5fd7766012
OmAxiani0 <aximaniom@gmail.com>
2022-08-08 02:54:50 +0530

    som changes

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,12 +1,17 @@
+import Link from "next/link"
+import Image from "next/image"
+
 export default function Navbar() {
   return (
     <>
       <nav className="bg-white border-b-2 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
         <div className="container flex flex-wrap justify-center items-center mx-auto">
-          <a href="/" className="flex items-center">
+          <Link href="/">
+            <a className="flex items-center">
             <img src='/preview.png' className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
               <span className="text-center text-xl font-semibold whitespace-nowrap dark:text-white">Previewer</span>
-          </a>
+              </a>
+          </Link>
         </div>
       </nav>
     </>
